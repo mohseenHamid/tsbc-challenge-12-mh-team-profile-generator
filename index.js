a541766fbe7bf4d519d648ce36ae382da65fb9aa
mohseenHamid <mhsn@hotmail.co.uk>
2023-03-08 16:50:51 +0000

    test app functionality with manual data - pass (generated HTML in output folder)

--- index.js
+++ index.js
@@ -18,22 +18,28 @@ const { init } = require("./lib/game-functions");
 /* 
 --- TASK: Write Code to gather information about the development team members, and render the HTML file ---
 
---- APP STEPS ---
+--- game-functions PAGE ---
 - create an empty team array in the global scope
 	- pass the employee objects from the inquirer responses into the "teamArray" array
 	- pass the "teamArray" array into the render function and assign it to a variable called renderResult
 	- the render function will render the HTML and call the generateTeam ftn with the "team" array as its argument
-	- renderResult will be the content input for the writeToFile ftn in the index.js page
-		- need to export renderResult from the game-functions page
-
-- write ftns for each employee type to prompt user for details
+	- renderResult will be the content input for the writeToFile ftn
+- Inquirer ftns
+	- write ftns for each employee type to prompt user for details
+		- the "then" for each employee type ftn should create the respective employee object (using employee type's class) based on the response data
+		- the employee object should be pushed to the global team array (that's later fed into the render ftn)
+		- call the menu ftn in the "then" section to return to menu upon completion
+	- write a menu ftn that prompts the user to select a move
+		- selecting an employee type should call that employee type ftn
+- write a quit ftn that stops the game if the "Finish building team" menu option is selected
+- write a init() ftn
+	- displays the welcome message
+	- initiates the app by calling the manager ftn
+	- calls the writeToFile ftn at the end
+		- the renderResult ftn from the game-functions page will be the content argument
 
 --- index.js PAGE ---
-- write a init() ftn in the index.js page
-	- displays the welcome message
-	- initiates the app by calling the game ftn
-	- calls the writeToFile ftn
-		- the renderResult ftn from the game-functions page will be the content argument
+- call the init ftn
  */
 
 // function call to initialize program
