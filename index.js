3b60e7c28248586dc199a1540be254b4955cd855
mohseenHamid <mhsn@hotmail.co.uk>
2023-03-08 17:47:57 +0000

    complete questions & game-functions

--- index.js
+++ index.js
@@ -2,18 +2,12 @@
 // const path = require("path");
 // const fs = require("fs");
 
-// const Manager = require("./lib/Manager");
-// const Engineer = require("./lib/Engineer");
-// const Intern = require("./lib/Intern");
 // const { info } = require("console");
-// const { welcomeMsg } = require("./lib/welcome-msg");
+const { welcomeMsg } = require("./lib/welcome-msg");
 // const { writeToFile } = require("./lib/write-to-file");
 // const render = require("./src/page-template.js");
-
-const { init } = require("./lib/game-functions");
-
-// const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const { teamArray, getManagerDetails } = require("./lib/game-functions");
+const { getManagerDetails } = require("./lib/game-functions");
 
 /* 
 --- TASK: Write Code to gather information about the development team members, and render the HTML file ---
@@ -32,15 +26,37 @@ const { init } = require("./lib/game-functions");
 	- write a menu ftn that prompts the user to select a move
 		- selecting an employee type should call that employee type ftn
 - write a quit ftn that stops the game if the "Finish building team" menu option is selected
+
+--- index.js PAGE ---
+- requirements
+	- welcomeMsg
+	- teamArray
+	- getManagerDetails ftn
+	- render
+	- writeToFile
 - write a init() ftn
 	- displays the welcome message
 	- initiates the app by calling the manager ftn
 	- calls the writeToFile ftn at the end
 		- the renderResult ftn from the game-functions page will be the content argument
-
---- index.js PAGE ---
 - call the init ftn
  */
 
 // function call to initialize program
+function init() {
+	console.info(welcomeMsg);
+
+	getManagerDetails();
+
+	// // define the file name param for the writeToFile ftn
+	// const OUTPUT_DIR = path.resolve(__dirname, "./output");
+	// const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+	// // define the content param for the writeToFile ftn
+	// const renderResult = render(teamArray);
+
+	// // call the writeToFile ftn
+	// writeToFile(outputPath, renderResult);
+}
+
 init();
